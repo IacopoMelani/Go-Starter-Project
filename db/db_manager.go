000eb058238c94c409da594afb5e9342ea390cd8
Iacopo Melani <iacopomelani4@gmail.com>
2019-04-18 22:50:44 +0200

    Modificata logica di inserimento/aggiornamento valori

--- db/db_manager.go
+++ db/db_manager.go
@@ -9,13 +9,19 @@ import (
 	"sync"
 )
 
+// Salvable - Interfaccia per permettere di generalizzare il salvataggio di un model sul database
+type Salvable interface {
+	GetSaveQuery() string
+	SetRecordID(id int)
+}
+
 const stringConnection = "root:root@tcp(127.0.0.1:3306)/test"
 
 var db *sql.DB
 
 var once sync.Once
 
-// GetConnection -
+// GetConnection - restituisce un'istanza di connessione al database
 func GetConnection() *sql.DB {
 
 	once.Do(func() {
@@ -33,4 +39,31 @@ func GetConnection() *sql.DB {
 	})
 
 	return db
+}
+
+// Save - Metodo che si occcupa del salvataggio fisico sul database
+func Save(s Salvable, params []interface{}) error {
+
+	db := GetConnection()
+
+	stmt, err := db.Prepare(s.GetSaveQuery())
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(params...)
+	if err != nil {
+		return err
+	}
+
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	if lastID > 0 {
+		s.SetRecordID(int(lastID))
+	}
+	return nil
 }
--- db/db_manager_test.go
+++ db/db_manager_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"Go-Starter-Project/models"
 	"testing"
 )
 
@@ -15,4 +16,36 @@ func TestGetConnection(t *testing.T) {
 		t.Error(err.Error())
 	}
 
+}
+
+// TestSave - Si occupa di testare il salvataggio(insert/update) di una struct che implementa l'interfaccia Salvable
+func TestSave(t *testing.T) {
+
+	u := models.User{}
+	u.Name = "Filippo"
+	u.Lastname = "Neri"
+	u.Gender = "M"
+
+	params := []interface{}{u.Name, u.Lastname, u.Gender}
+
+	err := Save(&u, params)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if u.RecordID <= 0 {
+		t.Error("Index errato, è minore di 0")
+	}
+
+	u.Name = "Mario"
+
+	params = []interface{}{u.Name, u.Lastname, u.Gender, u.RecordID}
+
+	lastID := u.RecordID
+
+	err = Save(&u, params)
+
+	if u.RecordID <= 0 || lastID != u.RecordID {
+		t.Error("Index errato, è minore di 0 oppure non corrisponde con il precedente")
+	}
 }
