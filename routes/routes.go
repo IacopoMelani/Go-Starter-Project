9dbaccf9831c0eba7382d3ae88bbf1a56a8f65c6
Iacopo Melani <iacopomelani4@gmail.com>
2019-08-28 00:29:43 +0200

    Iniziata implementazione jwt

--- routes/routes.go
+++ routes/routes.go
@@ -4,13 +4,25 @@ import (
 	"Go-Starter-Project/controllers"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // InitGetRoutes - Dichiara tutte le route GET
 func InitGetRoutes(e *echo.Echo) {
 	e.GET("user/all", controllers.GetAllUser)
+	r := e.Group("/restricted")
+
+	// Configure middleware with the custom claims type
+	config := middleware.JWTConfig{
+		Claims:     &controllers.JwtCustomClaims{},
+		SigningKey: []byte("bomba"),
+	}
+	r.Use(middleware.JWTWithConfig(config))
+	r.GET("/user/duration", controllers.GetDurataionUsers)
 	e.GET("user/duration", controllers.GetDurataionUsers)
 }
 
 // InitPostRoutes - Dichiara tutte le route POST
-func InitPostRoutes(e *echo.Echo) {}
+func InitPostRoutes(e *echo.Echo) {
+	e.POST("/user/login", controllers.Login)
+}
