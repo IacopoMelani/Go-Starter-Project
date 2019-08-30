43c36a5ed47cb9b2bc7854ff71a92e6e40ea0257
Iacopo Melani <iacopomelani4@gmail.com>
2019-08-31 01:09:21 +0200

    Allineamento jwt auth

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,11 +1,20 @@
 package controllers
 
 import (
+	"time"
+
 	durationdata "github.com/IacopoMelani/Go-Starter-Project/models/duration_data"
 	"github.com/IacopoMelani/Go-Starter-Project/models/table_record/table"
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/labstack/echo"
 )
+
+// JwtCustomClaims - Definisce il custom claims per la generazione del token
+type JwtCustomClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
 
 // GetAllUser - Restituisce tutti gli utenti
 func GetAllUser(c echo.Context) error {
@@ -37,5 +46,49 @@ func GetDurataionUsers(c echo.Context) error {
 		Success: true,
 		Message: "ok!",
 		Content: data.Content,
+	})
+}
+
+// Login - Si occupa di effettuare il login
+func Login(c echo.Context) error {
+
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username != "Mario" || password != "123456" {
+
+		return c.JSON(401, Response{
+			Status:  1,
+			Success: false,
+			Message: "Wrong credentials",
+		})
+	}
+
+	claims := &JwtCustomClaims{
+		"Mario",
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	generatedToken, err := token.SignedString([]byte("bomba"))
+	if err != nil {
+
+		return c.JSON(500, Response{
+			Status:  2,
+			Success: false,
+			Message: "Error",
+		})
+	}
+
+	return c.JSON(200, Response{
+		Status:  0,
+		Success: true,
+		Message: "ok!",
+		Content: echo.Map{
+			"token": generatedToken,
+		},
 	})
 }
