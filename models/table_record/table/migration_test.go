3568b3e5b097787112a3e993053e249e6db07384
Iacopo Melani <iacopomelani4@gmail.com>
2019-09-07 21:51:50 +0200

    Miglioramenti a TableRecord

---
+++ models/table_record/table/migration_test.go
@@ -0,0 +1,43 @@
+package table
+
+import (
+	"testing"
+
+	"github.com/IacopoMelani/Go-Starter-Project/db"
+
+	"github.com/subosito/gotenv"
+)
+
+func TestMigration(t *testing.T) {
+
+	gotenv.Load("./../../../.env")
+
+	db := db.GetConnection()
+	conn, err := db.Begin()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer conn.Rollback()
+
+	mName := "test_migration"
+
+	migration, err := InsertNewMigration(mName, 1)
+	if err != nil {
+		conn.Rollback()
+		t.Fatal(err.Error())
+	}
+
+	m := NewMigration()
+	m.New()
+
+	err = LoadMigrationByName(mName, m)
+	if err != nil {
+		conn.Rollback()
+		t.Fatal(err.Error())
+	}
+
+	if m.Name != migration.Name || m.Status != migration.Status {
+		conn.Rollback()
+		t.Fatal("Operazione di migrazione errata")
+	}
+}
