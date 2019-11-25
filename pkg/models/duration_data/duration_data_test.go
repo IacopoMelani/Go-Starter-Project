072ed8aaf7cb87b4ab96c7f27a140d3a34f65e5a
Iacopo Melani <iacopomelani4@gmail.com>
2019-11-26 00:49:51 +0100

    Aggiunto test per duration_data

---
+++ pkg/models/duration_data/duration_data_test.go
@@ -0,0 +1,73 @@
+package durationdata
+
+import (
+	"io"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/IacopoMelani/Go-Starter-Project/config"
+	"github.com/IacopoMelani/Go-Starter-Project/pkg/helpers/request"
+	"github.com/subosito/gotenv"
+)
+
+// DurationDataTest - Definisce una struct che implementa RemoteData
+type DurationDataTest struct{}
+
+var ddt *DurationData
+var onceUser sync.Once
+
+// GeDurationDataTest - Restituisce l'istanza di DurantionData relativo agli utenti
+func GeDurationDataTest() *DurationData {
+	onceUser.Do(func() {
+		ddt = new(DurationData)
+		ddt.SetDurationDataInterface(DurationDataTest{})
+		ddt.SetTimeToRefresh(config.GetInstance().UserTimeToRefresh)
+		ddt.Daemon()
+	})
+	return ddt
+}
+
+// EncodeQueryString - Si occupa di aggiungere i paratri dell'header alla request
+func (u DurationDataTest) EncodeQueryString(req *http.Request) {}
+
+// GetBody - Restituisce il corpo della request
+func (u DurationDataTest) GetBody() io.Reader {
+	return nil
+}
+
+// GetMethod - Restituisce il metodo della richiesta remota
+func (u DurationDataTest) GetMethod() string {
+	return "GET"
+}
+
+// GetURL - Restituisce la url della richiesta remota
+func (u DurationDataTest) GetURL() string {
+	return "https://randomuser.me/api/"
+}
+
+// HandlerData - Si occupa di eseguire la funzione di handler per ricevere i dati
+func (u DurationDataTest) HandlerData() (interface{}, error) {
+	content, err := request.GetRemoteData(u)
+	return content, err
+}
+
+func TestDurationData(t *testing.T) {
+	gotenv.Load("./../../../.env")
+
+	RegisterInitDurationData(GeDurationDataTest)
+
+	InitDurationData()
+	time.Sleep(2 * time.Second)
+
+	d := GeDurationDataTest()
+
+	d.StopDaemon()
+
+	d.SetContent(12, 3)
+
+	d.GetContent()
+
+	d.GetSafeContent()
+}
