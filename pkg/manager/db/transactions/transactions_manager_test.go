10305dfcb1f287b5a1370ae668117239fbf41bc5
Iacopo Melani <iacopomelani4@gmail.com>
2020-01-25 02:42:54 +0100

    Aggiunto test per transactions_manager

---
+++ pkg/manager/db/transactions/transactions_manager_test.go
@@ -0,0 +1,86 @@
+package transactions
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/subosito/gotenv"
+
+	"github.com/jmoiron/sqlx"
+
+	record "github.com/IacopoMelani/Go-Starter-Project/pkg/models/table_record"
+
+	"github.com/IacopoMelani/Go-Starter-Project/pkg/models/table_record/table"
+
+	"github.com/IacopoMelani/Go-Starter-Project/pkg/manager/db"
+)
+
+const testMigrationName = "FOFFO"
+
+func TestTransactionx(t *testing.T) {
+
+	if err := gotenv.Load("./../../../../.env"); err != nil {
+		t.Fatal("Errore caricamento configurazione")
+	}
+
+	WithTransactionx(db.GetConnection().(*sqlx.DB), func(tx db.SQLConnector) error {
+
+		_, err := table.LoadAllMigrations(tx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	err := WithTransactionx(db.GetConnection().(*sqlx.DB), func(tx db.SQLConnector) error {
+
+		newMigration := table.NewMigration(tx)
+
+		newMigration.Name = testMigrationName
+
+		if err := record.Save(newMigration); err != nil {
+			return err
+		}
+
+		migration := table.NewMigration(tx)
+		if err := table.LoadMigrationByName(testMigrationName, migration); err != nil {
+			return err
+		}
+
+		if migration.Name != testMigrationName {
+			t.Error("Fallimento caricamento migrazione")
+		}
+
+		return errors.New("Rollback")
+	})
+
+	if err == nil {
+		t.Error("err dovrebbe essere valorizzato")
+	}
+
+	defer func() {
+		if p := recover(); p == nil {
+			t.Error("Dovrebbe essere panic")
+		}
+	}()
+	WithTransactionx(db.GetConnection().(*sqlx.DB), func(tx db.SQLConnector) error {
+
+		newMigration := table.NewMigration(tx)
+
+		newMigration.Name = testMigrationName
+
+		if err := record.Save(newMigration); err != nil {
+			return err
+		}
+
+		migration := table.NewMigration(tx)
+		if err := table.LoadMigrationByName(testMigrationName, migration); err != nil {
+			return err
+		}
+
+		panic("Rollback")
+
+		return nil
+	})
+}
